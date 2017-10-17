e7ac00cdde24c8292bb071a40e3fa27393c1f0a9
Unknown <1cheaterok1@gmail.com>
2017-10-17 18:48:47 +0300

    Добавляет простой пример для проверки работы сканера #MW-13

--- src/mobile/app/src/main/java/zalanaar/github/com/mobilewaiter/MainActivity.java
+++ src/mobile/app/src/main/java/zalanaar/github/com/mobilewaiter/MainActivity.java
@@ -1,7 +1,18 @@
 package zalanaar.github.com.mobilewaiter;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +20,36 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Рисуем qr-code
+        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+        final Bitmap myBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.puppy);
+        myImageView.setImageBitmap(myBitmap);
+
+        // Создаём кнопку
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView txtView = (TextView) findViewById(R.id.txtContent);
+
+                BarcodeDetector detector =
+                        new BarcodeDetector.Builder(getApplicationContext())
+                                .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                                .build();
+                if (!detector.isOperational()) {
+                    txtView.setText("Could not set up the detector!");
+                    return;
+                }
+
+                Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+                SparseArray<Barcode> barcodes = detector.detect(frame);
+
+                Barcode thisCode = barcodes.valueAt(0);
+                txtView.setText(thisCode.rawValue);
+            }
+        });
     }
 }
